9418f61c314419b2ed118a6aafef9107356b7753
zakiralekperov <zakiralekperov1998@gmail.com>
2023-05-28 00:58:46 +0300

    Initial commit

---
+++ src/main/java/com/zakalek/twettir/TwettirApplication.java
@@ -0,0 +1,13 @@
+package com.zakalek.twettir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwettirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwettirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakalek/twettir/TwettirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakalek.twettir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwettirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
